2a2c92c4d361d2af83e0131f4f5ce3a4437ff324
Luan <luan.gabriel.cardoso@hotmail.com>
2020-03-18 12:52:07 -0300

    Aula sobre comandos do sistema de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def hello_blog(request):
+    lista = ['Django','Python','Git','Html','Banco de dados','Linux','Nginx','Uwsgi','Systemctl']
+    data = {'name':'Curso de Django 3','lista_tecnologias':lista}
+    return render(request,'index.html',data)
